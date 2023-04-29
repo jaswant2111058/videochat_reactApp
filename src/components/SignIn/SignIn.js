d3e00a8c8d4de9bfe3210323b5067bbd859cc371
Jaswant Kushwaha <114646646+jaswant2111058@users.noreply.github.com>
2023-04-29 23:26:35 +0530

    v2
    v2

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -4,7 +4,7 @@ import { useNavigate, Link } from "react-router-dom";
 import "./SignIn.css";
 
 const SignIn = () => {
-  const baseURL = "http://localhost:5000";
+  const baseURL = "https://videocall-ajjm.onrender.com";
   const navigate = useNavigate()
 
   const [user, setUser] = useState({
