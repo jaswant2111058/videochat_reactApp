95a9f34eefa797b7443e2cd7f994e20c7b6f9475
Jaswant Kushwaha <114646646+jaswant2111058@users.noreply.github.com>
2023-04-29 23:27:15 +0530

    v2

--- src/components/SignUp/otpVerification.js
+++ src/components/SignUp/otpVerification.js
@@ -4,7 +4,7 @@ import { useNavigate } from 'react-router-dom';
 import './otpveri.css'
 
 const OTP = ({email}) =>{
-    const baseURL = "http://localhost:5000"
+    const baseURL = "https://videocall-ajjm.onrender.com"
     const navigate = useNavigate()
     const [OTP,setOTP] = useState({
         email:email.email,
